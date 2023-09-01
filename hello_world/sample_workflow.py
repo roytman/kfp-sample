ae0cb3251bb632ce13fa15b2eed15bfd2a4f8734
Alexey Roytman <roytman@il.ibm.com>
2023-09-02 00:03:51 +0300

    Add Ray
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>

--- hello_world/sample_workflow.py
+++ hello_world/sample_workflow.py
@@ -1,6 +1,7 @@
 import kfp.components as comp
 import kfp.dsl as dsl
 from kfp_tekton.compiler import TektonCompiler
+from kubernetes import client as k8s_client
 
 # components
 start_op = comp.load_component_from_file("../components/startComponent.yaml")
@@ -11,26 +12,40 @@ stop_op = comp.load_component_from_file("../components/stopComponent.yaml")
     name="sample-pipeline",
 )
 def sample_pipeline(
-    name: str = "ktp-test",
+    name: str = "kfp-test",
     delay: int = 6, 
 ):
 
-    # invoke pipeline
-    name_from_start = start_op(
+    clean_up_task = stop_op(name=name)
+    clean_up_task.set_image_pull_policy("Always")
+    clean_up_task.add_env_variable(
+        k8s_client.V1EnvVar(
+            name="NAMESPACE",
+            value_from=k8s_client.V1EnvVarSource(
+                field_ref=k8s_client.V1ObjectFieldSelector(field_path="metadata.namespace")
+            ),
+        )
+    )
+
+    with dsl.ExitHandler(clean_up_task):
+        # invoke pipeline
+        start = start_op(
             name=name,
             delay=delay,
         )
-    # No cashing
-    name_from_start.execution_options.caching_strategy.max_cache_staleness = "P0D"
-    # image pull policy
-    name_from_start.set_image_pull_policy("Always")
-    
-    stop = stop_op(name_from_start.output)
-    # No cashing
-    stop.execution_options.caching_strategy.max_cache_staleness = "P0D"
-    # image pull policy
-    stop.set_image_pull_policy("Always")
-
+        # No cashing
+        start.execution_options.caching_strategy.max_cache_staleness = "P0D"
+        # image pull policy
+        start.set_image_pull_policy("Always")
+        # environment variables
+        start.add_env_variable(
+            k8s_client.V1EnvVar(
+                name="NAMESPACE",
+                value_from=k8s_client.V1EnvVarSource(
+                    field_ref=k8s_client.V1ObjectFieldSelector(field_path="metadata.namespace")
+                ),
+            )
+        )
 
 if __name__ == "__main__":
     # Compiling the pipeline
