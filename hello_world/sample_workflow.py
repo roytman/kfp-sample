f5d10e6703ff20d7ee2ec7895ccf4d986f80aac6
Alexey Roytman <roytman@il.ibm.com>
2023-09-02 12:08:49 +0300

    remove components

--- hello_world/sample_workflow.py
+++ hello_world/sample_workflow.py
@@ -3,10 +3,125 @@ import kfp.dsl as dsl
 from kfp_tekton.compiler import TektonCompiler
 from kubernetes import client as k8s_client
 
+def wait(delay: int):
+    import time
+    time.sleep(delay)
+
+# execute start Ray cluster
+def start_ray_cluster(
+    name: str,  # name of Ray cluster
+):
+    # Import pieces from codeflare-sdk
+    import os
+    import sys
+    from codeflare_sdk.cluster.auth import TokenAuthentication
+    from codeflare_sdk.cluster.cluster import Cluster, ClusterConfiguration
+
+    # get current namespace
+    ns = os.getenv("NAMESPACE", "default")
+    # change the current directory to ensure that we can write
+    os.chdir("/tmp")
+    print(f"Executing in namespace {ns}, current working directory is {os.getcwd()}")
+   
+    # Create authentication object for oc user permissions
+    secret_path = "/var/run/secrets/kubernetes.io/serviceaccount"
+    with open(f"{secret_path}/token", "r") as file:
+        token = file.read().rstrip()
+    auth = TokenAuthentication(
+        token=token, server="https://kubernetes.default:443", skip_tls=False, ca_cert_path=f"{secret_path}/ca.crt"
+    )
+    try:
+        auth.login()
+    except Exception as e:
+        print(f"Failed to log into openshift cluster, error {e}. Please check token/server values provided")
+        sys.exit(1)
+    print("successfully logged in")
+
+   
+    # Create and configure our cluster object (and appwrapper)
+    cluster = Cluster(
+        ClusterConfiguration(
+            name=name,
+            namespace=ns,
+        )
+    )
+    print(f"Configuration for Ray cluster {name} in namespace {ns} is created")
+
+    try:
+        import time
+        # bring up the cluster
+        cluster.up()
+        print(f"Creating Ray cluster {name} in namespace {ns}...")
+
+        # and wait for it being up
+        # Give it a moment, to avoid racing conditions
+        time.sleep(40)
+        cluster.wait_ready()
+        rc = cluster.details(print_to_console=False)
+        print("Ray cluster is ready")
+        print(rc)
+
+        # Get cluster connection point for job submission
+        ray_dashboard_uri = cluster.cluster_dashboard_uri()
+        ray_cluster_uri = cluster.cluster_uri()
+        print(f"Ray_cluster is at {ray_cluster_uri}")
+        print(f"Ray_cluster dashboard is at {ray_dashboard_uri}")
+
+        # wait to ensure that cluster is really up. Without this we can get ocasional 503 error
+        time.sleep(5)
+
+        # make sure all the nodes are up
+    except Exception as e:
+        print(f"Failed to init Ray cluster, error {e}")
+        sys.exit(1)
+
+# Cleans and shutdowns the Ray cluster
+def cleanup_ray_cluster(
+    name: str,  # name of Ray cluster
+):
+    import os
+    import sys
+
+    from codeflare_sdk.cluster.auth import TokenAuthentication
+    from codeflare_sdk.cluster.cluster import Cluster, ClusterConfiguration
+
+    # get current namespace
+    ns = os.getenv("NAMESPACE", "default")
+    # change the current directory to ensure that we can write
+    os.chdir("/tmp")
+    print(f"Executing in namespace {ns}, current working directory is {os.getcwd()}")
+
+    # Create authentication object for oc user permissions
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/token", "r") as file:
+        token = file.read().rstrip()
+    auth = TokenAuthentication(token=token, server="https://kubernetes.default:443", skip_tls=True)
+    try:
+        auth.login()
+    except Exception as e:
+        print(f"Failed to log into openshift cluster, error {e}. Please check token/server values provided")
+        sys.exit(1)
+    print("successfully logged in")
+    # Create and configure our cluster object (and appwrapper)
+    cluster = Cluster(
+        ClusterConfiguration(
+            name=name,
+            namespace=ns,
+        )
+    )
+    print(f"Configuration for Ray cluster {name} in namespace {ns} is created")
+    # delete cluster
+    print("All done. Cleaning up")
+    try:
+        cluster.down()
+    except Exception as e:
+        print(f"Failed to down the Ray cluster, error {e}. Please check that the {name} appwrapper is removed")
+        sys.exit(1)
+
+base_kfp_image = "docker.io/roytman/kfp-oc:0.0.1"
 # components
-start_op = comp.load_component_from_file("../components/startComponent.yaml")
-wait_op = comp.load_component_from_file("../components/waitComponent.yaml")
-stop_op = comp.load_component_from_file("../components/stopComponent.yaml")
+start_op = comp.func_to_container_op(func=start_ray_cluster, base_image=base_kfp_image)
+wait_op = comp.func_to_container_op(func=wait, base_image=base_kfp_image)
+stop_op = comp.func_to_container_op(func=cleanup_ray_cluster, base_image=base_kfp_image)
 
 
 @dsl.pipeline(
